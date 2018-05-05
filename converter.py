c69d7545a4eb7b537f333caf1f0bc3e03614b593
Vlad Precup <vprecup@veinteractive.com>
2018-05-05 13:47:22 +0200

    Add label converter

---
+++ converter.py
@@ -0,0 +1,11 @@
+envs = ['bus', 'cafe/restaurant', 'car', 'city_center', 'forest_path',  'grocery_store', 'home', 
+       'beach', 'library', 'metro_station', 'office', 'residential_area', 'train', 'tram', 'park']
+
+def labels_to_numeric(labels):
+    '''Convert a list of TUT Acoustic Scenes 2017 environment labels to zero-based indexes.'''
+    labels = [l.decode() for l in labels]
+    return [envs.index(l) for l in labels]
+
+def numeric_to_labels(class_indexes):
+    '''Convert a list of zero-based indexes to TUT Acoustic Scenes 2017 environment labels.'''
+    return [envs[ind] for ind in class_indexes]
