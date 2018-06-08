47252c974e72d27c4f2a08ee1d2ed09a4ca5022f
Vlad Precup <vprecup@veinteractive.com>
2018-06-08 11:49:24 +0200

    Added data collection logic - prep for lstm

--- converter.py
+++ converter.py
@@ -1,9 +1,10 @@
 envs = ['bus', 'cafe/restaurant', 'car', 'city_center', 'forest_path',  'grocery_store', 'home', 
        'beach', 'library', 'metro_station', 'office', 'residential_area', 'train', 'tram', 'park']
 
-def labels_to_numeric(labels):
+def labels_to_numeric(labels, decode=True):
     '''Convert a list of TUT Acoustic Scenes 2017 environment labels to zero-based indexes.'''
-    labels = [l.decode() for l in labels]
+    if decode:
+        labels = [l.decode() for l in labels]
     return [envs.index(l) for l in labels]
 
 def numeric_to_labels(class_indexes):
